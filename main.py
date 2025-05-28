1cf4487283b3e747319687929031ef8d36381184
Thiago <thiagodiasbispo@gmail.com>
2025-05-28 10:24:15 -0300

    Atualização de textos

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ st.write("# Análise do fator de capacidade das usinas eólicas e solares do Bra
 
 st.write("""O **fator de capacidade** de uma usina, seja eólica ou solar, é uma medida que expressa a relação entre a quantidade real de energia elétrica gerada por essa usina em um determinado período de tempo e a quantidade máxima de energia que ela poderia gerar se operasse continuamente em sua potência nominal durante todo esse período.
 
-A análise da capacidade prática (fator de capacidade real) é fundamental para o planejamento, operação e decisões de investimento no setor elétrico brasileiro. Sendo assim, o objetivo deste trabalho é ajudar no entendimento da capacidade real utilizada pela pelas usinas solares e eólicas, e **embasar empiricamente o planjemanento de expansão da rede, a avaliação econônima, a integação do sisteam e políticas públicas de regulação**, em parceria com um Engenheiro elétrico da ONS.
+A análise da capacidade prática (fator de capacidade real) é fundamental para o planeamento, operação e decisões de investimento no setor elétrico brasileiro. Sendo assim, o objetivo deste trabalho é ajudar no entendimento da capacidade real utilizada pela pelas usinas solares e eólicas, e **embasar empiricamente o planejamento de expansão da rede, a avaliação econômica, a integração do sistema e políticas públicas de regulação**, em parceria com um Engenheiro elétrico da ONS.
 
 Os dados são provenientes de dados abertos ONS: https://dados.ons.org.br/dataset/fator-capacidade-2/resource/775048d0-463c-4bdf-9373-4a371cb24a1c
 """)
